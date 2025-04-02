bd9924fdbfca9b4d355bca7808ec6b5c31d9bb8d
sarah-hbb <sarahabibi.89@gmail.com>
2025-04-02 18:23:16 +0200

    Add Layout wrapper for consistance style for all pages

---
+++ src/components/UI/Layout.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+const Layout: React.FC<Props> = ({ children }) => {
+  return (
+    <div className="bg-black min-vh-100 text-white p-5 d-flex justify-content-center align-items-center">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
