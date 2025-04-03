7ecd3b3aef9527837f63fa0a0e1d9abab3122889
sarah-hbb <sarahabibi.89@gmail.com>
2025-04-03 23:43:21 +0200

    Add home button to Layout component

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -1,11 +1,15 @@
 import React, { ReactNode } from "react";
+import { Link } from "react-router-dom";
 
 type Props = {
   children: ReactNode;
 };
 const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="bg-black min-vh-100 min-w-100 text-white p-5 d-flex justify-content-center align-items-center ">
+    <div className="bg-black min-vh-100 min-w-100 text-white p-5 d-flex flex-column justify-content-center align-items-center ">
+      <Link to={"/"}>
+        <i className="bi bi-house position-fixed p-5 fs-1 top-0 start-0 right-end text-lime"></i>
+      </Link>
       {children}
     </div>
   );
