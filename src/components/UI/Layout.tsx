165f6dab83748fa95e1bf2b1e580f5381841c4c1
sarah-hbb <sarahabibi.89@gmail.com>
2025-04-03 16:24:11 +0200

    Add same layout to all pages using Layouts

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="bg-black min-vh-100 text-white p-5 d-flex justify-content-center align-items-center">
+    <div className="bg-black min-vh-100 min-w-100 text-white p-5 d-flex justify-content-center align-items-center ">
       {children}
     </div>
   );
